47439333700af0f292e543954800aad30c4e1f42
alexandre.chaves <alexandre.chaves@LCET1119.cwbdis.pucpr.br>
2016-03-16 19:09:13 -0300

    Hello Ok!

---
+++ HelloWorld/src/main/java/br/silveiraalexand/bsi/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package br.silveiraalexand.bsi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/ola")
+	public String hello()
+	{
+		return "hello";
+	}
+	
+}
